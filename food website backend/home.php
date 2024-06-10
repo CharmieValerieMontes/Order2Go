bb6f9feb95d9fe5842735a71c82d6bf3be389b6a
Cham2426 <145363346+CharmieValerieMontes@users.noreply.github.com>
2024-06-10 12:48:58 +0800

    pabago nalang ng ibang kulay

--- food website backend/home.php
+++ food website backend/home.php
@@ -45,34 +45,34 @@ include 'components/add_cart.php';
 
          <div class="swiper-slide slide">
             <div class="content">
-               <span>order online</span>
-               <h3>delicious pizza</h3>
+               <span>Top Products</span>
+               <h3>Okinawa</h3>
                <a href="menu.html" class="btn">see menus</a>
             </div>
             <div class="image">
-               <img src="images/home-img-1.png" alt="">
+               <img src="images/b4.png" alt="">
             </div>
          </div>
 
          <div class="swiper-slide slide">
             <div class="content">
-               <span>order online</span>
-               <h3>chezzy hamburger</h3>
+               <span>Top Products</span>
+               <h3>Don Darko</h3>
                <a href="menu.html" class="btn">see menus</a>
             </div>
             <div class="image">
-               <img src="images/home-img-2.png" alt="">
+               <img src="images/b6.png" alt="">
             </div>
          </div>
 
          <div class="swiper-slide slide">
             <div class="content">
-               <span>order online</span>
-               <h3>rosted chicken</h3>
+               <span>Top Products</span>
+               <h3>Banoffee Frappe</h3>
                <a href="menu.html" class="btn">see menus</a>
             </div>
             <div class="image">
-               <img src="images/home-img-3.png" alt="">
+               <img src="images/b1.png" alt="">
             </div>
          </div>
 
@@ -86,94 +86,29 @@ include 'components/add_cart.php';
 
 <section class="category">
 
-   <h1 class="title">food category</h1>
+   <h1 class="title">Affeliated Shop</h1>
 
    <div class="box-container">
 
-      <a href="category.php?category=fast food" class="box">
-         <img src="images/cat-1.png" alt="">
-         <h3>fast food</h3>
+      <a href="category.php?category=bigbrew" class="box">
+         <img src="images/BigBrew.png" alt="">
+         <h3>Big Brew</h3>
       </a>
 
-      <a href="category.php?category=main dish" class="box">
-         <img src="images/cat-2.png" alt="">
-         <h3>main dishes</h3>
+      <a href="category.php?category=donmacchiatos" class="box">
+         <img src="images/Don Macchiatos.png" alt="">
+         <h3>Don Macchiatos</h3>
       </a>
 
-      <a href="category.php?category=drinks" class="box">
-         <img src="images/cat-3.png" alt="">
-         <h3>drinks</h3>
+      <a href="category.php?category=baabaa" class="box">
+         <img src="images/Baa Baa.png" alt="">
+         <h3>Baa Baa</h3>
       </a>
 
-      <a href="category.php?category=desserts" class="box">
-         <img src="images/cat-4.png" alt="">
-         <h3>desserts</h3>
-      </a>
 
    </div>
 
 </section>
-
-
-
-
-<section class="products">
-
-   <h1 class="title">latest dishes</h1>
-
-   <div class="box-container">
-
-      <?php
-         $select_products = $conn->prepare("SELECT * FROM `products` LIMIT 6");
-         $select_products->execute();
-         if($select_products->rowCount() > 0){
-            while($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)){
-      ?>
-      <form action="" method="post" class="box">
-         <input type="hidden" name="pid" value="<?= $fetch_products['id']; ?>">
-         <input type="hidden" name="name" value="<?= $fetch_products['name']; ?>">
-         <input type="hidden" name="price" value="<?= $fetch_products['price']; ?>">
-         <input type="hidden" name="image" value="<?= $fetch_products['image']; ?>">
-         <a href="quick_view.php?pid=<?= $fetch_products['id']; ?>" class="fas fa-eye"></a>
-         <button type="submit" class="fas fa-shopping-cart" name="add_to_cart"></button>
-         <img src="uploaded_img/<?= $fetch_products['image']; ?>" alt="">
-         <a href="category.php?category=<?= $fetch_products['category']; ?>" class="cat"><?= $fetch_products['category']; ?></a>
-         <div class="name"><?= $fetch_products['name']; ?></div>
-         <div class="flex">
-            <div class="price"><span>$</span><?= $fetch_products['price']; ?></div>
-            <input type="number" name="qty" class="qty" min="1" max="99" value="1" maxlength="2">
-         </div>
-      </form>
-      <?php
-            }
-         }else{
-            echo '<p class="empty">no products added yet!</p>';
-         }
-      ?>
-
-   </div>
-
-   <div class="more-btn">
-      <a href="menu.html" class="btn">veiw all</a>
-   </div>
-
-</section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 <?php include 'components/footer.php'; ?>
